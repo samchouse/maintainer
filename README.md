5235ab658b738b879513cdc5c1697f167f38c6dc
Samuel Corsi-House <chouse.samuel@gmail.com>
2023-01-02 22:32:28 -0500

    feat: deno

--- README.md
+++ README.md
@@ -1,33 +1,7 @@
-# maintainer
+# Maintainer
 
-> A GitHub App built with [Probot](https://github.com/probot/probot) that An app to manage your repositories automatically
+## Running
 
-## Setup
-
-```sh
-# Install dependencies
-npm install
-
-# Run the bot
-npm start
+```bash
+deno run --allow-env --allow-read --allow-net src/index.ts
 ```
-
-## Docker
-
-```sh
-# 1. Build container
-docker build -t maintainer .
-
-# 2. Start container
-docker run -e APP_ID=<app-id> -e PRIVATE_KEY=<pem-value> maintainer
-```
-
-## Contributing
-
-If you have suggestions for how maintainer could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
-
-For more, check out the [Contributing Guide](CONTRIBUTING.md).
-
-## License
-
-[ISC](LICENSE) © 2022 Samuel Corsi-House <chouse.samuel@gmail.com>
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,58 @@
-import { Probot } from "probot";
+import "https://deno.land/x/dotenv@v3.2.0/load.ts";
+import { App, createNodeMiddleware } from "https://esm.sh/octokit@2.0.7";
+import { serve } from "https://deno.land/std@0.157.0/http/mod.ts";
+import { EmitterWebhookEventWithStringPayloadAndSignature } from "https://esm.sh/v95/@octokit/webhooks@10.1.5/dist-types/types.d.ts";
 
-export = (app: Probot) => {
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    await context.octokit.issues.createComment(issueComment);
-  });
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+const config = {
+  appId: Deno.env.get("APP_ID"),
+  privateKey: Deno.env.get("PRIVATE_KEY"),
+  webhooks: {
+    secret: Deno.env.get("WEBHOOK_SECRET"),
+  },
+  oauth: {
+    clientId: Deno.env.get("CLIENT_ID"),
+    clientSecret: Deno.env.get("CLIENT_SECRET"),
+  },
 };
+
+if (
+  !config.appId || !config.privateKey || !config.webhooks.secret ||
+  !config.oauth.clientId || !config.oauth.clientSecret
+) {
+  throw new Error("Missing environment variables");
+}
+
+const app = new App({
+  appId: config.appId,
+  privateKey: config.privateKey,
+  webhooks: {
+    secret: config.webhooks.secret,
+  },
+  oauth: {
+    clientId: config.oauth.clientId,
+    clientSecret: config.oauth.clientSecret,
+  },
+});
+
+app.webhooks.on("installation.created", (e) => {
+  console.log("i.c", e);
+});
+
+app.webhooks.onAny((event) => {
+  console.log(event);
+});
+
+await serve(async (req) => {
+  const body = await req.text();
+
+  await app.webhooks.verifyAndReceive({
+    id: req.headers.get("x-github-delivery") ?? "",
+    name: req.headers.get(
+      "x-github-event",
+    ) as EmitterWebhookEventWithStringPayloadAndSignature["name"] ?? "",
+    signature: req.headers.get("x-hub-signature-256") ?? "",
+    payload: body,
+  });
+
+  return new Response("ok");
+}, { port: 3000 });
